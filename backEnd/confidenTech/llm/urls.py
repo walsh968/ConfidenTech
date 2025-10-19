b586d221b416bbb61368c8b7b9b678d59104b4ea
westinjiang <jyuanwei2000@gmail.com>
2025-10-19 19:10:39 -0400

    exposed data into exportable JSON/CSV format

--- backEnd/confidenTech/llm/urls.py
+++ backEnd/confidenTech/llm/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     
     # Add other AI-related URLs
     path("api/raw/", views.get_raw_outputs, name="raw-outputs"),
-    # path("api/raw/export/", views.export_raw_outputs, name="raw-exports"),
+    path("api/raw/export/", views.export_raw_outputs, name="raw-exports"),
+    path("exportConfidenceData", views.export_confidence_data, name="export-confidence-data"),
 ]
